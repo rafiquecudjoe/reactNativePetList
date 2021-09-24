d46f09ffb4eca6d635bbda9c559c2373240f8947
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2021-09-24 07:18:55 +0000

    new update

--- petList.js
+++ petList.js
@@ -5,12 +5,12 @@ import Card from './components/card'
 export default function PetList() {
 
     const pets = [
-        { name: 'Pascal', category: 'cat', age: "2 years old", address: "95 Shoreline Dr_" ,image:require("./assets/pet1.jpg")},
-        { name: 'Raka', category: 'dog', age: "1.3 years old", address: "97 Shoreline Dr_",image:require("./assets/pet2.jpg") },
-        { name: 'Jhony', category: 'dog', age: "3 years old", address: "9h Shoreline Dr_", image:require("./assets/pet3.jpg")},
-        { name: 'Dogger', category: 'dog', age: "2 years old", address: "98 Shoreline Dr_",image:require("./assets/pet4.jpg") },
-        {name:'Hata',category:'dog',age:"5 years old", address:"93 Shoreline Dr_",image:require("./assets/pet5.jpg")},
-        {name:'Body',category:'dog',age:"6 years old", address:"93 Shoreline Dr_",image:require("./assets/pet6.jpg")},
+        { name: 'Pascal', category: 'Cat', age: "2 years old", address: "95 Shoreline Dr_" ,image:require("./assets/pet1.jpg")},
+        { name: 'Raka', category: 'Dog', age: "1.3 years old", address: "97 Shoreline Dr_",image:require("./assets/pet2.jpg") },
+        { name: 'Jhony', category: 'Dog', age: "3 years old", address: "9h Shoreline Dr_", image:require("./assets/pet3.jpg")},
+        { name: 'Dogger', category: 'Dog', age: "2 years old", address: "98 Shoreline Dr_",image:require("./assets/pet4.jpg") },
+        {name:'Hata',category:'Dog',age:"5 years old", address:"93 Shoreline Dr_",image:require("./assets/pet5.jpg")},
+        {name:'Body',category:'Dog',age:"6 years old", address:"93 Shoreline Dr_",image:require("./assets/pet6.jpg")},
     ]
     return (
         <View>
