8ad71ae17314db50b31cb8bf22cf2063de126ac3
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2021-09-24 18:57:38 +0000

    new update

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View,TextInput } from 'react-native';
-import PetList from './petList';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, TextInput } from "react-native";
+import PetList from "./petList";
+import { EvilIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.sarea}>
-       
-      <TextInput style={styles.search} placeholder="Search Here"/>
-
+      <View style={styles.search}>
+        <EvilIcons name="search" size={24} color="black" />
+        <TextInput style={styles.input }placeholder="Search Here" />
       </View>
-      
 
-      <PetList/>
+      <PetList />
       <StatusBar style="auto" />
     </View>
   );
@@ -22,19 +20,24 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical:50,
+    paddingVertical: 50,
     flex: 1,
-    backgroundColor: '#E6E6E6',
- 
+    backgroundColor: "#E6E6E6",
   },
   search: {
-   backgroundColor: 'white',
+    backgroundColor: "white",
     marginHorizontal: 50,
     height: 50,
     borderRadius: 10,
-    alignItems: 'center',
-    paddingLeft:40
-
+    alignItems: "center",
+    paddingLeft: 5,
+    flexDirection: "row",
+    alignItems:"center"
   },
-  
+  input: {
+    height: 50,
+    flex: 1,
+    paddingLeft: 10,
+    borderWidth:0
+  }
 });
